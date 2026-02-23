0f72d46e529630c2d0764ded77dee41c7a33fb7a
Sunil <sunilmallela9177@gmail.com>
2026-02-23 17:25:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AddEmp from "./components/AddEmp"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+  <>
+  <Navbar/>
+  <AddEmp/>
+  </>
+  )
+}
+
+export default App
